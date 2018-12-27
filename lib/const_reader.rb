6d55634a66b4b4f16eb8810764cb6c10609a649b
Mehmet Emin INAC <mehmetemininac@gmail.com>
2018-12-27 14:30:52 +0300

    Initial commit

---
+++ lib/const_reader.rb
@@ -0,0 +1,6 @@
+require "const_reader/version"
+
+module ConstReader
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/const_reader/version.rb
@@ -0,0 +1,3 @@
+module ConstReader
+  VERSION = "0.1.0"
+end
